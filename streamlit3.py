34a51a4d6cc3f80d74f7ce6f1f617c1cb0cd09fd
cmunhall <coopermunhall@gmail.com>
2022-08-27 13:20:08 -0400

    nowpls

--- streamlit3.py
+++ streamlit3.py
@@ -8,9 +8,9 @@ import entprestige3
 st.title("Network Visualization of ENT Programs #1-10")
 
 st.sidebar.title('Programs #1-10 Network Graph')
-option=st.sidebar.selectbox('select graph',('Programs #1-10 Network Graph'))
+option=st.sidebar.selectbox('select graph',('Programs #1-10 Network Graph',"nothing"))
 
 if option=='Programs #1-10 Network Graph':
   HtmlFile = open("testentgraph.html", 'r', encoding='utf-8')
   source_code = HtmlFile.read() 
-  components.html(source_code, height = 900,width=900)
+  components.html(source_code, height = 1500,width=1500)
