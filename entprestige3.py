45f1e5b9b1ea1b27e7969b31c23591fc21f1ebea
cmunhall <coopermunhall@gmail.com>
2022-08-27 12:47:12 -0400

    gimme

--- entprestige3.py
+++ entprestige3.py
@@ -1,3 +1,4 @@
+from optparse import Values
 from pyvis.network import Network
 from pyvis.network import Network
 net = Network(height='80%', width='80%')
@@ -8,6 +9,7 @@ import streamlit.components.v1 as components
 
 st.title("Network Visualization of Top 10 ENT Programs")
 
+
 data = pd.read_csv('~/Desktop/prestigedata.csv')
 
 sources = data['source']
@@ -16,15 +18,16 @@ weights = data['weight']
 size = data['audience.size']
 label = data['label']
 
-edge_data = zip(sources, targets, weights)
+edge_data = zip(sources, targets, weights, size)
 
 for e in edge_data:
     src = e[0]
     dst = e [1] 
     w = e[2]
+    asize = e[3]
 
-    net.add_node(src, src, title=src)
-    net.add_node(dst, dst, title=dst)
+    net.add_node(src, src, value=asize, title=src)
+    net.add_node(dst, dst, value=asize, title=dst)
     net.add_edge(src, dst, value=w)
 
 
