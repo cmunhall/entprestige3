fea104e9f918f30203e619f2d0a7554b1d7b5c9d
cmunhall <coopermunhall@gmail.com>
2022-11-16 11:19:39 -0500

    go

--- ENTstreamlitTest.py
+++ ENTstreamlitTest.py
@@ -7,6 +7,6 @@ st.sidebar.title('Programs #1-10')
 option=st.sidebar.selectbox('select graph',('Programs #1-10 Network Graph',"None"))
 
 if option=='Programs #1-10 Network Graph':
-  HtmlFile = open("https://github.com/cmunhall/entprestige3/blob/master/foo2.html", 'r', encoding='utf-8')
+  HtmlFile = open("foo2.html", 'r', encoding='utf-8')
   source_code = HtmlFile.read() 
-  components.html(source_code, height = 1000,width=800)
+  components.html(source_code, height = 1000,width=1000)
